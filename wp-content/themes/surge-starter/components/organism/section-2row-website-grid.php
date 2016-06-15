03e77d1e92fd20935c83ae8c3d5e9a773841237c
Hector <hectornucv@gmail.com>
2016-06-15 13:30:06 +1000

    Group of website related cards built.

--- wp-content/themes/surge-starter/components/organism/section-2row-website-grid.php
+++ wp-content/themes/surge-starter/components/organism/section-2row-website-grid.php
@@ -1,14 +1,97 @@
-<section class="" style="background-image: url('<?php echo $vars['background'] ?>');">
+<section class="text-center" style="background-image: url('<?php echo $vars['background'] ?>');">
     <div class="container">
     <h1>
         <?php echo $vars['title'] ?>
     </h1>
+ 
+
+<?php
+      //debug($vars);
+      /*=============================================
+      = Card (Class,Image,Title,Content)
+      = @components
+        + molecule/card
+      =============================================*/
+      get_component([ 'template' => 'molecule/card',
+                      'remove_tags' => ['img'],
+                      'vars' => [
+                            "class" => 'col-md-6 card',
+                            "title" => $vars["title"],
+                            "subtitle" => $vars["subtitle"],
+                            "content" => apply_filters('the_content',  $vars["content"]),
+                              "button" => get_component([
+                                'template' => 'atom/link',
+                                'return_string' => true,
+                                'vars' => [
+                                      "class" => 'btn text-uppercase pull-left',
+                                      "text" => $vars['button'][0]['text'],
+                                      "url" => $vars['button'][0]['link'],
+                                      ]
+                                ])
+                            ]
+                       ]);
+?>
+
    <?php
 
   // WP_Query arguments
 $args = array (
   "post_type" => "any",
-  "post__in" => $vars['first_row']
+  "post__in" => $vars['upcoming']
+);
+
+// The Query
+$query = new WP_Query( $args );
+
+// The Loop
+if ( $query->have_posts() ) {
+  while ( $query->have_posts() ) {
+    $query->the_post();?>
+
+<article class="upcoming card">
+  <hgroup>
+    <h1>Upcoming Workshop</h1>
+    <small><?php echo get_the_title(); ?></small>
+    <!-- <small><?php echo get_field('location') ?></small> -->
+    <h1>Remaining Seats: <?php// echo get_field('seats') ?> </h1>
+  </hgroup>
+  <?php 
+        get_component([
+                'template' => 'atom/link',
+                'vars' => [
+                  "class" => 'btn text-uppercase',
+                  "text" => "overview",
+                  "url" => "",
+                  ]
+        ]);
+         get_component([
+                'template' => 'atom/link',
+                'vars' => [
+                  "class" => 'btn text-uppercase',
+                  "text" => "book now",
+                  "url" => "",
+                  ]
+        ]);
+                                 ?>
+
+</article>
+
+<?php  }
+} else {
+  // no posts found`
+}
+
+// Restore original Post Data
+wp_reset_postdata();
+    ?>
+
+
+   <?php
+
+  // WP_Query arguments
+$args = array (
+  "post_type" => "any",
+  "post__in" => $vars['second_row']
 );
 
 // The Query
@@ -39,7 +122,7 @@ if ( $query->have_posts() ) {
                        ]);
   }
 } else {
-  // no posts found
+  // no posts found`
 }
 
 // Restore original Post Data
@@ -51,7 +134,7 @@ wp_reset_postdata();
 // WP_Query arguments
 $args = array (
   "post_type" => "any",
-  "post__in" => $vars['second_row']
+  "post__in" => $vars['third_row']
 );
 
 // The Query
