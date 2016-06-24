c60cadf058de89ee99f5d942ba5983c7dc683be0
Alex <alexanderddking@gmail.com>
2016-06-24 16:40:01 +1000

    Single Program Page

---
+++ wp-content/themes/surge-starter/single-program.php
@@ -0,0 +1,49 @@
+<?php while (have_posts()) : the_post(); ?>
+<?php 
+
+
+	//debug(get_post_meta(get_the_id()));
+
+
+$vars = [
+	'title' => get_field('title'),
+	'subtitle' => get_field('subtitle'),
+	'paragraphs' => get_field('paragraphs'),
+	'paragraphs_size' => sizeof(get_field('paragraphs')),
+	'form' => get_field('form'),
+	'form_footer' => get_field('form_footer'),
+	'bottom_title' => get_field('bottom_title'),
+	'bottom_textarea' => get_field('bottom_textarea'),
+	'background_image' => get_field('image'),
+	'color' => get_field('color'),
+	];
+	debug($vars);
+ ?>
+
+<section class="" style="background-image:url(<?php  echo $vars['background_image']; ?>)">
+	
+	<div class="overlay container">
+		<hgroup>
+			<h6><?php echo $vars['subtitle']; ?></h6>
+			<h1><?php echo $vars['title']; ?></h1>
+		</hgroup>
+		<?php 
+
+		for ($vars['i']=0; $vars['i'] < ; $vars['i']++) { 
+
+			 			get_component([ 'template' => 'molecule/card',
+											'remove_tags'=> ['img','h6',],
+											'vars' => [
+														"class" => 'col-md-6 card '+$vars['class'],
+														"title" => $vars["title"]$vars['i'],
+														"subtitle" => $vars["subtitle"],
+														"content" => apply_filters('the_content',  $vars["content"]),
+														"button" => $vars['button']
+														]
+											 ]);
+		} ?>
+	</div>
+
+</section>
+
+<?php endwhile; ?>
